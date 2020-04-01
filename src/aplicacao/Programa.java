8a49cb9491e374465332c79450e58683cc46d2d1
Renan Klinsmann <klismann-nan@hotmail.com>
2020-04-01 15:39:18 -0300

    Projeto finalizado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,5 +1,7 @@
 package aplicacao;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -12,37 +14,49 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Bem vindo a biblioteca! ");
-		System.out.println("Digite seus dados ->");
-		System.out.print("Nome: ");
-		String nome = sc.nextLine();
-		System.out.print("Matricula: ");
-		Integer matricula = sc.nextInt();
-		System.out.print("Curso: ");
-		sc.nextLine();
-		String curso = sc.nextLine();
-		Usuario usuario = new Usuario(nome, matricula, curso);
-		System.out.println("-------------------------------");
-		System.out.println("Quantidadede alugueis se limita a 3 livros por usuario");
-		System.out.println("Quantos livros deseja usar? ");
-		int n = sc.nextInt();
+		List<Usuario> list = new ArrayList<>();
+		try {
+			System.out.println("Quantos Usuarios deseja cadastrar? ");
+			int n = sc.nextInt();
+			for (int i = 0; i <= n; i++) {
 
-		for (int i = 1; i <= n; i++) {
+				System.out.println("Bem vindo a biblioteca! ");
+				System.out.println("Digite seus dados ->");
+				sc.nextLine();
+				System.out.print("Nome: ");
+				String nome = sc.nextLine();
+				System.out.print("Matricula: ");
+				Integer matricula = sc.nextInt();
+				System.out.print("Curso: ");
+				sc.nextLine();
+				String curso = sc.nextLine();
+				Usuario usuario = new Usuario(nome, matricula, curso);
+				System.out.println("-------------------------------");
+				System.out.println("Quantidade de alugueis se limita a 3 livros por usuario");
+				System.out.println("Quantos livros deseja usar? ");
+				int n2 = sc.nextInt();
 
-			System.out.println("Digite os dados do livro para prosseguir: ");
-			System.out.println("Titulo do livro: ");
-			sc.nextLine();
-			String titulo = sc.nextLine();
-			System.out.println("Codigo ISBN: ");
-			Integer isbn = sc.nextInt();
-			Livro livro = new Livro(titulo, isbn);
-			usuario.addQuantLivros(livro);
+				for (int j = 1; j <= n2; j++) {
+
+					System.out.println("Digite os dados do livro para prosseguir: ");
+					System.out.println("Titulo do livro: ");
+					sc.nextLine();
+					String titulo = sc.nextLine();
+					System.out.println("Codigo ISBN: ");
+					Integer isbn = sc.nextInt();
+					Livro livro = new Livro(titulo, isbn);
+					usuario.addQuantLivros(livro);
+					System.out.println("-------------------------------");
+				}
+			}
 			System.out.println("-------------------------------");
-
+			for (Usuario obj : list) {
+				System.out.println(obj);
+			}
+		} catch (RuntimeException e) {
+			System.out.println("Erro Inesperado!");
+			System.out.println("Reinicie o Programa e verifique se esta fazendo correto!");
 		}
-		System.out.println("-------------------------------");
-		System.out.println(usuario);
-
 		sc.close();
 	}
 
